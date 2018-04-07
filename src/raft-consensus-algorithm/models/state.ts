6c5e71854123db4e2a62062a80680389d0c3ef20
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-04-07 13:41:10 +0200

    update

--- src/raft-consensus-algorithm/models/state.ts
+++ src/raft-consensus-algorithm/models/state.ts
@@ -3,6 +3,7 @@ import { LogEntry } from './log-entry';
 export class State {
 
     constructor(
+        public commitIndex: number,
         public currentTerm: number,
         public isCandidate: boolean,
         public isFollower: boolean,
@@ -16,4 +17,19 @@ export class State {
     public lastLogEntryIndex(): number {
         return this.logEntries.length === 0 ? 0 : this.logEntries[this.logEntries.length - 1].index;
     }
+
+    public setAsCandidate(): void {
+        this.commitIndex = this.lastLogEntryIndex();
+        this.isCandidate = true;
+        this.isFollower = false;
+        this.isLeader = false;
+    }
+
+    public setAsFollower(): void {
+        this.commitIndex = this.lastLogEntryIndex();
+        this.isCandidate = false;
+        this.isFollower = true;
+        this.isLeader = false;
+    }
+
 }
--- src/raft-consensus-algorithm/raft-consensus-algorithm.ts
+++ src/raft-consensus-algorithm/raft-consensus-algorithm.ts
@@ -1,5 +1,6 @@
 import { AppendEntriesRequest } from './models/append-entries-request';
 import { AppendEntriesResponse } from './models/append-entries-response';
+import { LogEntry } from './models/log-entry';
 import { State } from './models/state';
 import { VoteRequest } from './models/vote-request';
 import { VoteResponse } from './models/vote-response';
@@ -8,9 +9,9 @@ export class RaftConsensusAlgorithm {
 
     protected electionTimeout: number = null;
 
-    protected sendHeartbeat: (state: State) => void = null;
+    protected sendHeartbeat: (logEntries: LogEntry[], state: State) => Promise<boolean> = null;
 
-    protected state: State = new State(0, false, true, false, [], null);
+    protected state: State = new State(0, 0, false, true, false, [], null);
 
     public handleAppendEntriesRequest(appendEntriesRequest: AppendEntriesRequest): AppendEntriesResponse {
         if (this.state.currentTerm < appendEntriesRequest.term) {
@@ -19,6 +20,7 @@ export class RaftConsensusAlgorithm {
 
         if (this.state.currentTerm > appendEntriesRequest.term) {
             this.state.currentTerm = appendEntriesRequest.term;
+            this.state.setAsFollower();
         }
 
         if (!appendEntriesRequest.logEntries || appendEntriesRequest.logEntries.length === 0) {
@@ -28,6 +30,8 @@ export class RaftConsensusAlgorithm {
         if (this.state.lastLogEntryIndex() + 1 !==  appendEntriesRequest.logEntries[0].index) {
             return new AppendEntriesResponse(this.state.currentTerm, false);
         }
+
+        this.state.logEntries = this.state.logEntries.concat(appendEntriesRequest.logEntries);
     }
 
     public handleVoteRequest(voteRequest: VoteRequest): VoteResponse {
@@ -37,6 +41,7 @@ export class RaftConsensusAlgorithm {
 
         if (this.state.currentTerm > voteRequest.term) {
             this.state.currentTerm = voteRequest.term;
+            this.state.votedFor = null;
         }
 
         if (this.state.votedFor) {
@@ -49,6 +54,30 @@ export class RaftConsensusAlgorithm {
     }
 
     public tick(): void {
+        if (this.state.isCandidate) {
+
+        } else if (this.state.isFollower) {
+
+        } else if (this.state.isLeader) {
+            this.tickLeader();
+        }
+    }
+
+    protected hasExceededElectionTime(): boolean {
+        return new Date().getTime() > this.electionTimeout;
+    }
+
+    protected resetElectionTimeout(): void {
+        this.electionTimeout = new Date().getTime() + Math.floor(Math.random() * 9000) + 1000;
+    }
+
+    protected tickFollower(): void {
+        if (this.hasExceededElectionTime()) {
+
+        }
+    }
+
+    protected tickLeader(): void {
 
     }
 
