d443b509b333c2352d2f3926503378c20c4c948b
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-04-14 09:49:20 +0200

    update

--- src/brute-force-password-cracker/app.ts
+++ src/brute-force-password-cracker/app.ts
@@ -3,6 +3,6 @@ import { BruteForcePasswordCrackerAgent } from './agent';
 export let bruteForcePasswordCrackerAgent: BruteForcePasswordCrackerAgent = null;
 
 (async () => {
-    bruteForcePasswordCrackerAgent = new BruteForcePasswordCrackerAgent('ws://localhost:5001');
+    bruteForcePasswordCrackerAgent = new BruteForcePasswordCrackerAgent('ws://138.68.188.8:5001');
     await bruteForcePasswordCrackerAgent.connect();
 })();
